d26a639c4eba71d4d9319fea620c8b948d86cebd
_godiah_ <51189464+godiah@users.noreply.github.com>
2025-06-01 17:53:09 +0300

    Project set-up

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mustard Adventures</title>
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1 class="text-amber-800">Welcome</h1>
+</body>
+
+</html>
